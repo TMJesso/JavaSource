a3a85398c74d9755cb4a19d4c75a89ab138dc08a
TMJesso <Jess_Hort_Farms@mail.com>
2015-03-06 04:08:50 -0500

    Change everything
    Change the names of the source copy funciton

--- Javagithub.bat
+++ Javagithub.bat
@@ -2,5 +2,5 @@
 d:
 xcopy D:\Data\Github\Java\JavaSource\*.java D:\Data\Programming\Java\Spring2015 /s /y /q
 xcopy D:\Data\Github\Java\JavaSource\*.dic D:\Data\Programming\Java\Spring2015 /s /y /q
-xcopy D:\Data\Github\Java\JavaSource\githubJava.bat D:\Data\Programming\Java\Spring2015 /s /y /q
-xcopy D:\Data\Github\Java\JavaSource\Javagithub.bat D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\To.bat D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\From.bat D:\Data\Programming\Java\Spring2015 /s /y /q
--- githubJava.bat
+++ githubJava.bat
@@ -2,5 +2,5 @@
 d:
 xcopy D:\Data\Programming\Java\Spring2015\*.java D:\Data\Github\Java\JavaSource /s /y /q
 xcopy D:\Data\Programming\Java\Spring2015\*.dic D:\Data\Github\Java\JavaSource /s /y /q
-xcopy D:\Data\Programming\Java\Spring2015\githubJava.bat D:\Data\Github\Java\JavaSource /s /y /q
-xcopy D:\Data\Programming\Java\Spring2015\Javagithub.bat D:\Data\Github\Java\JavaSource /s /y /q
+xcopy D:\Data\Programming\Java\Spring2015\To.bat D:\Data\Github\Java\JavaSource /s /y /q
+xcopy D:\Data\Programming\Java\Spring2015\From.bat D:\Data\Github\Java\JavaSource /s /y /q
