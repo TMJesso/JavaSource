7299658d661dbd420069b6699713804ccc70b743
TMJesso <Jess_Hort_Farms@mail.com>
2015-03-06 03:56:56 -0500

    Change copy function
    Copy the change in the copy function

--- githubJava.bat
+++ githubJava.bat
@@ -1,4 +1,4 @@
-@echo off
+echo off
 d:
 xcopy D:\Data\Programming\Java\Spring2015\*.java D:\Data\Github\Java\JavaSource /s /y
 xcopy D:\Data\Programming\Java\Spring2015\*.dic D:\Data\Github\Java\JavaSource /s /y
