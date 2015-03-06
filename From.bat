2a174539ede4f91739ec704665d9aaf0916bf9a1
TMJesso <Jess_Hort_Farms@mail.com>
2015-03-06 04:12:27 -0500

    Now the changes take
    Now the changes take shape and are enforced

---
+++ From.bat
@@ -0,0 +1,6 @@
+@echo off
+d:
+xcopy D:\Data\Github\Java\JavaSource\*.java D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\*.dic D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\To.bat D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\From.bat D:\Data\Programming\Java\Spring2015 /s /y /q
