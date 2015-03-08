649f74f8f0fed9cf77bf44e003f57ed8c454ed2f
TMJesso <Jess_Hort_Farms@mail.com>
2015-03-08 17:37:37 -0400

    Remove copyfrom
    Remove the copy from file because the source needs to be able to be
    imported into Java from the source location

--- CopyFrom.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-d:
-xcopy D:\Data\Github\Java\JavaSource\*.java D:\Data\Programming\Java\Spring2015 /s /y /q
-xcopy D:\Data\Github\Java\JavaSource\*.dic D:\Data\Programming\Java\Spring2015 /s /y /q
-xcopy D:\Data\Github\Java\JavaSource\CopyTo.bat D:\Data\Programming\Java\Spring2015 /s /y /q
-xcopy D:\Data\Github\Java\JavaSource\CopyFrom.bat D:\Data\Programming\Java\Spring2015 /s /y /q
