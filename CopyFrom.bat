82410ac42c61cd2ef38f56e4c48c31c04539d8ef
TMJesso <Jess_Hort_Farms@mail.com>
2015-03-14 18:51:15 -0400

    Revert "Update"
    This reverts commit a2584f52f22e3b474a2be64029ee92a100976adc.

--- CopyFrom.bat
+++ CopyFrom.bat
@@ -3,3 +3,4 @@ d:
 xcopy D:\Data\Github\Java\JavaSource\*.java D:\Data\Programming\Java\Spring2015 /s /y /q
 xcopy D:\Data\Github\Java\JavaSource\*.dic D:\Data\Programming\Java\Spring2015 /s /y /q
 xcopy D:\Data\Github\Java\JavaSource\CopyTo.bat D:\Data\Programming\Java\Spring2015 /s /y /q
+xcopy D:\Data\Github\Java\JavaSource\CopyFrom.bat D:\Data\Programming\Java\Spring2015 /s /y /q
